7d572125cad30ec230832eb1b5a1c8d514671a70
zsi <9818138@gmail.com>
2025-01-16 08:43:40 +0200

    Add division operation

--- calculator.cpp
+++ calculator.cpp
@@ -12,5 +12,10 @@ int Calculator::Sub (double a, double b)
 
 int Calculator::Mul (double a, double b)
 {
-    return a * b;
+	return a * b + 0.5;
+}
+
+int Calculator::Div (double a, double b)
+{
+	return a / b + 0.5;
 }
